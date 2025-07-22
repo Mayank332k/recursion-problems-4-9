a6b4602033e5f6586cbe91f5147e3155f5073f00
Mayank Singh <singhmayank4146@gmail.com>
2025-07-22 12:35:24 +0530

    unique subsequences...

--- susbsequences.java
+++ susbsequences.java
@@ -1,21 +1,27 @@
+import java.util.HashSet;
 public class susbsequences {
-    public static void subseq( int idx ,String str , String newString  ){
-
+    public static void subseq( int idx ,String str , String newString , HashSet set){
         if(idx == str.length()){
-            System.out.println(newString);
-            return;
+            if(set.contains(newString)){
+                return;
+            } else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
         }
         char currChar = str.charAt(idx);
         // if aana hai
-        subseq(idx+1, str, newString+currChar);
+        subseq(idx+1, str, newString+currChar , set);
 
         // if nhi aaana hai !
-        subseq(idx+1, str, newString);
+        subseq(idx+1, str, newString , set);
 
         
     }
     public static void main(String[] args) {
-        String str = "abc";
-        subseq(0, str, "");
+        String str = "aaa";
+        HashSet<String> set = new HashSet<>();
+        subseq(0, str, "", set);
     }
 }
